6bd90e423617dea8bde84e288b2c79b008b79375
Luiza <Marques>
2020-07-09 17:30:12 -0300

    Refactoring game code

--- Estudos/alion/game.js
+++ Estudos/alion/game.js
@@ -1,5 +1,3 @@
-// Variables
-
 const buttonColors = ["orange", "blue", "green", "pink"];
 let randomNumber;
 let randomChosenColor;
@@ -8,162 +6,165 @@ let userPattern = [];
 let started = false;
 let level = 0;
 
-//  Functions
-
 //* Disable buttons before game starts
 
 if (!started) {
     $(".btn").addClass("no-clicks");
 }
 
-//* START THE GAME
+//* Press Space to Start
 
 $(document).keypress(function (event) {
 
     if (!started) {
         if (event.which == 32) {
 
-            startGame();
-
+            nextSequence();
         }
     }
 })
 
-//* RUN AND PLAY THE NEXT SEQUENCE
 
+//! Cria uma nova sequencia
 function nextSequence() {
-    console.log("userPattern: " + userPattern.length);
 
+    started = true;
     userPattern = [];
-    level++;
 
-    $("#level-title").text("Level " + (level));
-
+    // cria um número de 1 a 4 e adiciona ao array gamePattern
     randomNumber = Math.floor(Math.random() * 4);
     randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    playSequence();
+    level++; // sobe o level
 
+    $("#level-title").text("Level " + (level)); // atualiza o level no título
+
+
+    // diminui em x por partida o delay entre as animações
+
+    var delay = 700;
+    var difficulty = 30;
+
+    delay = delay - ((gamePattern.length - 1) * difficulty);
+    animTime = (delay / 1000) + "s";
+    $(".btn").css("transitionDuration", animTime);
+
+    // Toca a sequencia com intervalo definido por time
+
+    var counter = 0;
+
+    var timer = setInterval(() => {
+
+        if (gamePattern.length > counter) {
+            //acertou
+
+            var pressedBtn = ("." + gamePattern[counter]);
+            var color = (gamePattern[counter]);
+
+            newblink(pressedBtn, color);
+
+            counter++;
+
+        } else {
+            // errou
+
+            clearInterval(timer);
+        }
+
+    }, delay);
 }
 
-//* Add a timer between loop iterations so the sequence can be fully played.back
 
-function timer(ms) {
-    return new Promise(res => setTimeout(res, ms));
-}
+//! Adiciona o input do usuário ao array e dispara a função blink quando o usuário clica
 
-async function playSequence() {
+$(".btn").click((event) => {
+    var pressedBtn = event.target; // define o div que foi clickado
+    var pressedColor = $(pressedBtn).attr("id"); // seleciona a cor pelo id do div
 
-    for (let i = 0; i < gamePattern.length; i++) {
+    userPattern.push(pressedColor); // adiciona ao array userPattern
 
-        const button = gamePattern[i];
+    newblink(pressedBtn, pressedColor); // dispara a animação e som
 
-        blink(button);
-        playSound(button);
+    //* checar entrada do jogador
 
-        await timer(1000);
-    }
-}
+    //  se o valor do index do array do jogador bate com o do jogo
 
+    if (gamePattern[userPattern.length - 1] === userPattern[userPattern.length - 1]) {
+        console.log("acertou");
 
-
-//* WATCH PLAYER INPUT
-
-$('.btn').click(function (event) {
-    const pressedBtn = event.target;
-    const pressedColor = pressedBtn.classList[1];
-
-    userPattern.push(pressedColor);
-
-    playSound(pressedColor);
-    blink(pressedColor);
-
-    checkAnswer(userPattern.length - 1);
-})
-
-
-//* CHECK ANSWER
-
-function checkAnswer(currentLevel) {
-
-    if (gamePattern[currentLevel] === userPattern[currentLevel]) {
-        console.log("Yes!");
-
+        // ate os dois terem o mesmo tamanho
         if (userPattern.length === gamePattern.length) {
+
             setTimeout(function () {
+                console.log("próximo level");
+
+
                 nextSequence();
-            }, 1000);
+
+            }, 500);
         }
 
     } else {
 
-        console.log("NOOO!");
-        destroyGame();
+        console.log("Errou");
 
+        sound = new Audio("/estudos/alion/sounds/wrong.wav")
+        sound.play();
+
+        const title = $("#level-title");
+
+        title.text("GAME OVER");
+        title.addClass("game-over");
+
+        $(".btn").addClass("destroyed");
+        $(".btn").on("transitionend", function () {
+
+            $(".btn").css("display", "none");
+            $("h2").addClass("restart");
+        })
+
+
+        $(document).keypress((event) => {
+
+            if (started) {
+                if (event.which == 32) {
+                    console.log("Restart Game")
+
+                    $(".btn").css("display", "inline-block");
+                    $(".btn").removeClass("destroyed");
+                    $(".btn").removeClass("no-clicks");
+                    $("h2").removeClass("restart");
+                    $("#level-title").removeClass("game-over");
+
+                    console.log("Novo jogo.")
+
+                    started = false;
+                    level = 0;
+                    gamePattern = [];
+
+                    nextSequence();
+                }
+            }
+        })
     }
-}
 
 
-function blink(btn) {
+})
 
-    const pressedBtn = $("." + btn);
-    const pressedClass = btn + "-pressed";
-    pressedBtn.addClass(pressedClass);
-    pressedBtn.on("animationend", function () {
-        pressedBtn.removeClass(pressedClass);
+function newblink(pressedBtn, pressedColor) {
+
+    //* Tocar o Audio
+    var sound = new Audio("/estudos/alion/sounds/" + pressedColor + ".wav"); // cria o nome do arquivo de audio
+    sound.play(); // toca o audio selecionado
+
+    //* Disparar a animação
+    var pressedClass = `${pressedColor}-pressed`;
+    $(pressedBtn).addClass(pressedClass); // adiciona a classe cor-pressed
+
+    //* Aguarda o fim da animação e remove a classe
+    $(pressedBtn).on("animationend", () => {
+        $(pressedBtn).removeClass(pressedClass);
     })
-}
 
-function playSound(btn) {
-
-    const sound = new Audio("/Estudos/alion/sounds/" + btn + (".mp3"))
-    sound.play();
-    console.log("BLINNNK")
-
-}
-
-function destroyGame() {
-
-    const title = $("#level-title");
-
-    title.text("GAME OVER");
-    title.addClass("game-over");
-
-    $(".btn").addClass("destroyed");
-    $(".btn").on("transitionend", function () {
-        $(".btn").css("display", "none");
-        $("h2").addClass("restart");
-    })
-    started = false;
-    userPattern = [];
-
-
-}
-
-function restartGame() {
-
-    $(".btn").css("display", "inline-block");
-    $(".btn").removeClass("destroyed");
-    $(".btn").removeClass("no-clicks");
-    $("h2").removeClass("restart");
-    $("#level-title").removeClass("game-over");
-
-    userPattern.length = 0;
-    gamePattern.length = 0;
-    level = 0;
-
-
-}
-
-
-function startGame() {
-
-    restartGame();
-    started = true;
-    $("#level-title").text("Level " + (level + 1));
-
-    setTimeout(function () {
-        nextSequence();
-    }, 500);
 }
